c31bd29fcfeca9dd043174dccadc91a3103c4dcb
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2023-08-30 23:32:26 -0400

    feat: finish code for page-zero

--- src/page-zero.asm
+++ src/page-zero.asm
@@ -2,7 +2,9 @@
 ;; The iNES header (contains a total of 16 bytes with the flags)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .segment "HEADER"
-.org $7FF0
+.org $7FF0                  ; $7FF0 is 16 bytes before $8000 so it is placing the header
+                            ; before the bytes of PRG-ROM
+
 .byte $4E,$45,$53,$1A       ; 4 bytes with the chars 'N', 'E', 'S', '\n'
 .byte $02                   ; How many 16KB of PRG-ROM we'll use (32KB)
 .byte $01                   ; How many 8KB of CHR-ROM we'll use (8KB)
@@ -12,3 +14,34 @@
 .byte $00                   ; NTSC TV Format
 .byte $00                   ; No PGR-RAM
 .byte $00,$00,$00,$00,$00   ; Unused padding to complete 16bytes of the header
+
+.segment "CODE"
+.org $8000 ; where the program ROM is mapped
+
+RESET:
+    ; TODO what happens when we reset
+    sei         ; Disable all IRQ interrupts
+    cld         ; Clear decimal mode flag (not supported by NES)
+    ldx #$FF    ; Initialize the stack pointer at $01FF (bottom of the stack)
+    txs 
+
+    lda #0      ; A = 0
+    ldx #$FF    ; start in the last address X = $FF
+MemLoop:
+    sta $0,x    ; stores 0 at "index" of X
+    dex         ; decrement X register, X--
+    bne MemLoop ; If x is not zero, we loop back to MemLoop
+
+NMI:            ; NMI Handler
+    rti ; return
+IRQ:            ; IRQ handler
+    rti ; return
+
+
+; 6502 processor will always go to $FFFA first and check for
+; vectors that are addresses that point to the handlers
+.segment "VECTORS"
+.org $FFFA
+.word NMI   ; address of the NMI handler 16bits
+.word RESET ; address of the RESET handler 16bits
+.word IRQ   ; address of the IRQ handler 16bits
